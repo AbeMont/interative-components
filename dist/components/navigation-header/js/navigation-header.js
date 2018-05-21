5c24dd6b578926c275f09e3ebc437908901f17ed
unknown <abraham.montoya1990@gmail.com>
2018-05-20 22:46:23 -0500

    Main header navigation mobile menu initiated

---
+++ dist/components/navigation-header/js/navigation-header.js
@@ -0,0 +1,22 @@
+'use strict';
+
+console.log('Navigation Header');
+
+var openMobileMenu = document.querySelector('.header .header__desktop [data-open-sideNav]');
+var closeMobileMenu = document.querySelector('.header .header__desktop [data-close-sideNav]');
+
+openMobileMenu.addEventListener('click', function () {
+  toggleIcon(this, closeMobileMenu);
+});
+
+closeMobileMenu.addEventListener('click', function () {
+  toggleIcon(this, openMobileMenu);
+});
+
+function toggleIcon(hideElement, showElement) {
+  if (hideElement.classList.contains('active')) {
+    hideElement.classList.remove('active');
+    showElement.classList.add('active');
+  }
+}
+//# sourceMappingURL=navigation-header.js.map

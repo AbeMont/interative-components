25b62ff56198dc5bd23d7529d14049c83a8362dc
unknown <abraham.montoya1990@gmail.com>
2018-05-21 20:42:25 -0500

    z-index fix on header mobile when class is active

--- dist/components/navigation-header/js/navigation-header.js
+++ dist/components/navigation-header/js/navigation-header.js
@@ -4,7 +4,9 @@ console.log('Navigation Header');
 
 var openMobileMenu = document.querySelector('.header .header__desktop [data-open-sideNav]');
 var closeMobileMenu = document.querySelector('.header .header__desktop [data-close-sideNav]');
-
+var mobileHeader = document.querySelector('.header__mobile');
+console.log(mobileHeader);
+console.log(sideNav);
 openMobileMenu.addEventListener('click', function () {
   toggleIcon(this, closeMobileMenu);
 });
@@ -14,6 +16,16 @@ closeMobileMenu.addEventListener('click', function () {
 });
 
 function toggleIcon(hideElement, showElement) {
+
+  if (!mobileHeader.classList.contains('active')) {
+    console.log('I have it slow me down');
+    mobileHeader.classList.add('active');
+  } else {
+    setTimeout(function () {
+      mobileHeader.classList.remove('active');
+    }, 400);
+  }
+
   if (hideElement.classList.contains('active')) {
     hideElement.classList.remove('active');
     showElement.classList.add('active');

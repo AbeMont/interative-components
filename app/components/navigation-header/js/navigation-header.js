ceff707b8f5f4c9d2faaabbe38f52878e619ad92
unknown <abraham.montoya1990@gmail.com>
2018-05-28 01:02:37 -0500

    SideNav logic complete for navigation header

--- app/components/navigation-header/js/navigation-header.js
+++ app/components/navigation-header/js/navigation-header.js
@@ -4,7 +4,9 @@ var openMobileMenu = document.querySelector('.header .header__desktop [data-open
 var closeMobileMenu = document.querySelector('.header .header__desktop [data-close-sideNav]');
 var mobileHeader = document.querySelector('.header__mobile');
 var mainHeaderList = Array.from(document.querySelector('.header__main ul').children);
-console.log(mainHeaderList);
+var headerSideNav = document.querySelector('.header__mobile .side-nav-accordion');
+var headerHoverMenu = Array.from(document.querySelectorAll('.header__hover-menu'));
+console.log(headerHoverMenu);
 
 openMobileMenu.addEventListener('click', function(){
   toggleIcon(this,closeMobileMenu);
@@ -30,10 +32,60 @@ function toggleIcon(hideElement,showElement){
 
 mainHeaderList.forEach(function(e){
   var listAnchor = e.children[0];
-  console.log(listAnchor);
   var subMenu = e.children[0].nextElementSibling;
-  console.log(subMenu);
   if(subMenu){
-
+    e.classList.add('header__subnav--active');
+    listAnchor.addEventListener('click', function(){
+      console.log(this);
+    });
   }
 });
+
+var sideNavList = function () {
+
+  var ul = document.createElement('ul');
+  var sideNavUl = headerSideNav.appendChild(ul);
+
+  mainHeaderList.forEach(function(listItem){
+
+    var li = document.createElement('li');
+    var a = document.createElement('a');
+    var listText = listItem.children[0].textContent;
+
+    var sideNavAnchor = sideNavUl.appendChild(li).appendChild(a);
+    sideNavAnchor.href = listItem.children[0].href;
+    sideNavAnchor.textContent = listText;
+
+    var subMenu = listItem.children[0].nextElementSibling;
+
+    if(subMenu){
+      sideNavAnchor.setAttribute('data-toggle-subnav','');
+      var i = document.createElement('i');
+      var newAnchor = sideNavAnchor.appendChild(i);
+      newAnchor.classList.add('fa','fa-angle-down');
+
+      var ul = document.createElement('ul');
+      var subnav = sideNavAnchor.parentNode.insertBefore(ul,sideNavAnchor.nextElementSibling);
+      subnav.classList.add('sub-section');
+
+      headerHoverMenu.forEach(function(el){
+        if (el.parentNode.children[0].textContent === sideNavAnchor.textContent) {
+
+          for (var i = 0; i < el.children.length; i++) {
+            var li = document.createElement('li');
+            var a = document.createElement('a');
+            var href = el.children[i].children[0].href;
+            var innerSubNav = subnav.appendChild(li).appendChild(a);
+            var innerSubNavText = el.children[i].children[0].textContent;
+            innerSubNav.href = href;
+            innerSubNav.textContent = innerSubNavText;
+          }
+        }
+      });
+    }
+
+  });
+
+}
+
+sideNavList();
